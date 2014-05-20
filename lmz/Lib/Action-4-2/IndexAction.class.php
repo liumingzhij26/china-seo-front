a7fb384413a00bff4401c1d89c6781e27cb96087
lmz <610852772@qq.com>
2014-05-20 13:56:36 +0800

    update file

--- lmz/Lib/Action-4-2/IndexAction.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-// 本类由系统自动生成，仅供测试用途
-class IndexAction extends BaseAction {
-	
-	public function index() {	
-		header("Location:http://www.hotelclub.com/");
-		//$this->show('<h2>首页暂时不提供访问！</h2><h3> home page temporarily does not provide access! </h3>','utf-8');
-	}
-}
